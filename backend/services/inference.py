fd9c67da81a199e1decffeffab405ae3b819aaea
Dzoixy <sittichaisc1@gmail.com>
2026-03-18 14:04:01 +0700

    add services inference

---
+++ backend/services/inference.py
@@ -0,0 +1,37 @@
+import torch
+import cv2
+import numpy as np
+from torchvision import models, transforms
+from utils.preprocess import preprocess
+
+model = models.resnet18()
+model.fc = torch.nn.Linear(model.fc.in_features, 2)
+model.load_state_dict(torch.load("models/lung_model.pt"))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+async def run_inference(file):
+    data = await file.read()
+
+    img = cv2.imdecode(
+        np.frombuffer(data,np.uint8),
+        cv2.IMREAD_COLOR
+    )
+
+    img = preprocess(img)
+    img = transform(img).unsqueeze(0)
+
+    with torch.no_grad():
+        out = model(img)
+        pred = torch.argmax(out,1).item()
+        conf = torch.softmax(out,1).max().item()
+
+    return {
+        "prediction": "PNEUMONIA" if pred else "NORMAL",
+        "confidence": float(conf)
+    }
